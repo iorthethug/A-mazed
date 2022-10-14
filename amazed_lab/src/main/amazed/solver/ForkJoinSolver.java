6cc4143976e19d083dff1a605ed46852e2ff42c9
jonathann000 <97891351+jonathann000@users.noreply.github.com>
2022-10-14 12:09:24 +0200

    skapaentrådgår

--- amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
+++ amazed_lab/src/main/amazed/solver/ForkJoinSolver.java
@@ -6,8 +6,10 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.ForkJoinPool;
 
 /**
  * <code>ForkJoinSolver</code> implements a solver for
@@ -18,18 +20,15 @@ import java.util.concurrent.ConcurrentSkipListSet;
  * <code>ForkJoinPool</code> object.
  */
 
-
 public class ForkJoinSolver
-    extends SequentialSolver
-{
+        extends SequentialSolver {
     /**
      * Creates a solver that searches in <code>maze</code> from the
      * start node to a goal.
      *
-     * @param maze   the maze to be searched
+     * @param maze the maze to be searched
      */
-    public ForkJoinSolver(Maze maze)
-    {
+    public ForkJoinSolver(Maze maze) {
         super(maze);
     }
 
@@ -38,14 +37,13 @@ public class ForkJoinSolver
      * start node to a goal, forking after a given number of visited
      * nodes.
      *
-     * @param maze        the maze to be searched
-     * @param forkAfter   the number of steps (visited nodes) after
-     *                    which a parallel task is forked; if
-     *                    <code>forkAfter &lt;= 0</code> the solver never
-     *                    forks new tasks
+     * @param maze      the maze to be searched
+     * @param forkAfter the number of steps (visited nodes) after
+     *                  which a parallel task is forked; if
+     *                  <code>forkAfter &lt;= 0</code> the solver never
+     *                  forks new tasks
      */
-    public ForkJoinSolver(Maze maze, int forkAfter)
-    {
+    public ForkJoinSolver(Maze maze, int forkAfter) {
         this(maze);
         this.forkAfter = forkAfter;
     }
@@ -57,18 +55,66 @@ public class ForkJoinSolver
      * goals, or all goals are unreacheable), the method returns
      * <code>null</code>.
      *
-     * @return   the list of node identifiers from the start node to a
-     *           goal node in the maze; <code>null</code> if such a path cannot
-     *           be found.
+     * @return the list of node identifiers from the start node to a
+     *         goal node in the maze; <code>null</code> if such a path cannot
+     *         be found.
      */
     @Override
-    public List<Integer> compute()
-    {
+    public List<Integer> compute() {
+
         return parallelSearch();
     }
 
-    private List<Integer> parallelSearch()
-    {
+    private List<Integer> parallelSearch() {
+        // one player active on the maze at start
+        int player = maze.newPlayer(start);
+        // start with start node
+        frontier.push(start);
+        // as long as not all nodes have been processed
+        while (!frontier.empty()) {
+            // get the new node to process
+            int current = frontier.pop();
+            // if current node has a goal
+            if (maze.hasGoal(current)) {
+                // move player to goal
+                maze.move(player, current);
+                // search finished: reconstruct and return path
+                return pathFromTo(start, current);
+            }
+            // if current node has not been visited yet
+            if (!visited.contains(current)) {
+                // move player to current node
+                maze.move(player, current);
+                // mark node as visited
+                visited.add(current);
+                // for every node nb adjacent to current
+                for (int nb : maze.neighbors(current)) {
+                    if (frontier.size() > 1) {
+                        ForkJoinSolver task = new ForkJoinSolver(maze);
+
+                        task.visited = visited;
+                        task.start = nb;
+
+                        System.out.println(nb);
+                        System.out.println(visited);
+
+                        task.fork();
+                        if (task.join() == null) {
+                        } else
+                            return task.join();
+
+                    }
+                    // add nb to the nodes to be processed
+                    frontier.push(nb);
+                    // if nb has not been already visited,
+                    // nb can be reached from current (i.e., current is nb's predecessor)
+                    if (!visited.contains(nb))
+                        predecessor.put(nb, current);
+                }
+            }
+        }
+        // all nodes explored, no goal found
         return null;
     }
+
 }
